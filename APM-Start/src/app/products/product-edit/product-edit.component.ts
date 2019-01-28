a0fd33cff2b3ac1566003708942bf174a8b8e7f9
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-01-28 12:38:55 +0200

    Reading Route Parameters: Snapshot Demo

--- APM-Start/src/app/products/product-edit/product-edit.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { MessageService } from '../../messages/message.service';
 
@@ -9,14 +10,20 @@ import { ProductService } from '../product.service';
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.css']
 })
-export class ProductEditComponent {
+export class ProductEditComponent implements OnInit {
   pageTitle = 'Product Edit';
   errorMessage: string;
 
   product: Product;
 
   constructor(private productService: ProductService,
-              private messageService: MessageService) { }
+              private messageService: MessageService,
+              private route: ActivatedRoute) { }
+
+    ngOnInit() {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.getProduct(id);
+    }
 
   getProduct(id: number): void {
     this.productService.getProduct(id)
