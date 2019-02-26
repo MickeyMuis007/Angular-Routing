8c5607577f0b6e92ade01fb69faf5212dca63adf
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-02-26 16:30:21 +0200

    Building a Route Resolver Service

---
+++ APM-Start/src/app/products/product-resolver.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductResolver implements Resolve<Product> {
+    constructor(private productService: ProductService) { }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+        const id = +route.paramMap.get('id');
+        return this.productService.getProduct(id);
+    }
+}
