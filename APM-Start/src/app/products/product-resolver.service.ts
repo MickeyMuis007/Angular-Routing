25873f712119dd5e9af07a00a0ccea355ca32e91
Michael Alan Hendricks <michaelh@lawforall.co.za>
2019-02-27 07:30:13 +0200

    Adding a Resolver to a Route Configuration

--- APM-Start/src/app/products/product-resolver.service.ts
+++ APM-Start/src/app/products/product-resolver.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
-import { Product } from './product';
+import { Product, ProductResolved } from './product';
 import { ProductService } from './product.service';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ProductResolver implements Resolve<Product> {
+export class ProductResolver implements Resolve<ProductResolved> {
     constructor(private productService: ProductService) { }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ProductResolved> {
         const id = route.paramMap.get('id');
         if (isNaN(+id)) {
-            const message = 'Product id was not a number: ${id}';
+            const message = `Product id was not a number: ${id}`;
             console.error(message);
-            return; // ???
+            return of({product: null, error: message});
         }
-        return this.productService.getProduct(+id);
+        return this.productService.getProduct(+id)
+            .pipe(
+                map(product => ({product: product })),
+                catchError(error => {
+                    const message = `Retrieval error: ${error}`;
+                    console.error(message);
+                    return of({product: null, error: message});
+                })
+            );
     }
 }
